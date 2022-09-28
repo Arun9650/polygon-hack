0055810e7f5d1c4a09a0e88909c297d93bc2a7f9
Arun9650 <arun02580@gmail.com>
2022-09-28 20:45:18 +0530

    add env file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ module.exports = {
   images: {
     domains: ["ipfs.io","ninetheme.com","www1.djicdn.com","stormsend1.djicdn.com","github.com","www2.djicdn.com","www4.djicdn.com","raw.githubusercontent.com"],
   },
+  env:{
+    api_key:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDU0ZkMxMTU0NzY3MzBCQTEyODBEYUFFRjNBODcxZGFGYzc0ZTdBQjkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MzE0NDMyODEzNSwibmFtZSI6InBvbHlnb24tbmZ0In0.aqieFyTXDvAAbdNGBdwVu3ePNBkW3bvUCeUrw_uslXI"
+  }
 
 
     
